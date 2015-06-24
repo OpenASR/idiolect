fcb90d77f9361b4d1e022a6259b1a519ada20970
breandan <breandan.considine@gmail.com>
2015-06-23 20:41:27 -0400

    Add toolbar button and more speech commands.

--- src/com/jetbrains/idear/Idear.java
+++ src/com/jetbrains/idear/Idear.java
@@ -1,103 +1,25 @@
 package com.jetbrains.idear;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.components.ApplicationComponent;
-import edu.cmu.sphinx.api.Configuration;
-import edu.cmu.sphinx.api.LiveSpeechRecognizer;
 import org.jetbrains.annotations.NotNull;
-
-import java.awt.*;
-import java.io.IOException;
 
 /**
  * Created by breandan on 6/22/2015.
  */
 public class Idear implements ApplicationComponent {
 
-    private static final String ACOUSTIC_MODEL = "resource:/edu.cmu.sphinx.models.en-us/en-us";
-    private static final String DICTIONARY_PATH = "resource:/edu.cmu.sphinx.models.en-us/cmudict-en-us.dict";
-    private static final String GRAMMAR_PATH = "resource:/com.jetbrains.idear/dialog/";
-
-    private Configuration configuration;
-    private LiveSpeechRecognizer speechRecognizer;
 
     public Idear() {
-        configuration = new Configuration();
-        configuration.setAcousticModelPath(ACOUSTIC_MODEL);
-        configuration.setDictionaryPath(DICTIONARY_PATH);
-        configuration.setGrammarPath(GRAMMAR_PATH);
-        configuration.setUseGrammar(true);
-
-        configuration.setGrammarName("dialog");
-        try {
-            speechRecognizer = new LiveSpeechRecognizer(configuration);
-        } catch (IOException e) {
-            System.err.println("Couldn't initialize speech recognizer.");
-        }
     }
 
     @Override
     public void initComponent() {
-        speechRecognizer.startRecognition(true);
-        new Thread(() -> {
-            while (true) {
-                String utterance = speechRecognizer.getResult().getHypothesis();
 
-                System.out.println("User said: " + utterance);
-
-                if (utterance.startsWith("open")) {
-                    if (utterance.endsWith("settings")) {
-                            invokeAction(IdeActions.ACTION_SHOW_SETTINGS);
-                    }
-                }
-
-
-                if (utterance.startsWith("focus")) {
-                    if (utterance.endsWith("editor")) {
-                        invokeAction(IdeActions.ACTION_EDITOR_ESCAPE);
-                    } else if (utterance.endsWith("project")) {
-                        invokeAction(IdeActions.GROUP_PROJECT_VIEW_POPUP);
-                    }
-                }
-
-                if (utterance.endsWith("selection")) {
-                    if (utterance.startsWith("expand")) {
-                        invokeAction(IdeActions.ACTION_EXPAND_REGION_RECURSIVELY);
-                    } else if (utterance.startsWith("shrink")) {
-                        invokeAction(IdeActions.ACTION_COLLAPSE_REGION_RECURSIVELY);
-                    }
-                }
-
-                if (utterance.startsWith("speech pause")) {
-                    while (true) {
-                        utterance = speechRecognizer.getResult().getHypothesis();
-                        if (utterance.equals("speech resume")) {
-                            break;
-                        }
-                    }
-                }
-            }
-        }).start();
-    }
-
-    private void invokeAction(final String actionShowSettings) {
-        try {
-            EventQueue.invokeAndWait(() -> {
-                AnAction aa = ActionManager.getInstance().getAction(actionShowSettings);
-                aa.actionPerformed(new AnActionEvent(null,
-                        DataManager.getInstance().getDataContext(),
-                        ActionPlaces.UNKNOWN, aa.getTemplatePresentation(),
-                        ActionManager.getInstance(), 0));
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     public void disposeComponent() {
-        speechRecognizer.stopRecognition();
+
     }
 
     @Override
---
+++ src/com/jetbrains/idear/VoiceControlAction.java
@@ -0,0 +1,177 @@
+package com.jetbrains.idear;
+
+import com.intellij.ide.DataManager;
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.keymap.Keymap;
+import com.intellij.openapi.keymap.KeymapManager;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+
+/**
+ * Created by breandan on 6/23/2015.
+ */
+public class VoiceControlAction extends AnAction {
+    private final Thread speechThread = new Thread(new ControlLoop());
+
+    private static final String ACOUSTIC_MODEL = "resource:/edu.cmu.sphinx.models.en-us/en-us";
+    private static final String DICTIONARY_PATH = "resource:/edu.cmu.sphinx.models.en-us/cmudict-en-us.dict";
+    private static final String GRAMMAR_PATH = "resource:/com.jetbrains.idear/dialog/";
+    private Keymap keymap;
+
+    private Configuration configuration;
+    private LiveSpeechRecognizer speechRecognizer;
+    private Robot robot;
+    private boolean stopped = false;
+
+    public VoiceControlAction() {
+        init();
+    }
+
+    public VoiceControlAction(Icon icon) {
+        super(icon);
+        init();
+    }
+
+    public VoiceControlAction(String text) {
+        super(text);
+        init();
+    }
+
+    public VoiceControlAction(String text, String description, Icon icon) {
+        super(text, description, icon);
+        init();
+    }
+
+    public void init() {
+        configuration = new Configuration();
+        configuration.setAcousticModelPath(ACOUSTIC_MODEL);
+        configuration.setDictionaryPath(DICTIONARY_PATH);
+        configuration.setGrammarPath(GRAMMAR_PATH);
+        configuration.setUseGrammar(true);
+
+        keymap = KeymapManager.getInstance().getActiveKeymap();
+        configuration.setGrammarName("dialog");
+
+        try {
+            speechRecognizer = new LiveSpeechRecognizer(configuration);
+        } catch (IOException e) {
+            System.err.println("Couldn't initialize speech recognizer.");
+        }
+
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent actionEvent) {
+        if (!speechThread.isAlive()) {
+            stopped = false;
+            speechRecognizer.startRecognition(true);
+            speechThread.start();
+        } else {
+            speechRecognizer.stopRecognition();
+            stopped = true;
+        }
+    }
+
+    private class ControlLoop implements Runnable {
+        @Override
+        public void run() {
+            while (!stopped) {
+                String result = speechRecognizer.getResult().getHypothesis();
+                System.out.println("User said: " + result);
+
+                if (result.startsWith("open")) {
+                    if (result.endsWith("settings")) {
+                        invokeAction(IdeActions.ACTION_SHOW_SETTINGS);
+                    } else if (result.endsWith("recent")) {
+                        invokeAction(IdeActions.ACTION_RECENT_FILES);
+                    } else if (result.endsWith("terminal")) {
+                        pressKeystroke(KeyEvent.VK_ALT, KeyEvent.VK_F12);
+                    }
+                } else if (result.startsWith("focus")) {
+                    if (result.endsWith("editor")) {
+                        pressKeystroke(KeyEvent.VK_ESCAPE);
+                    } else if (result.endsWith("project")) {
+                        pressKeystroke(KeyEvent.VK_ALT, KeyEvent.VK_1);
+                    }
+                } else if (result.endsWith("selection")) {
+                    if (result.startsWith("expand")) {
+                        pressKeystroke(KeyEvent.VK_CONTROL, KeyEvent.VK_W);
+                    } else if (result.startsWith("shrink")) {
+                        pressKeystroke(KeyEvent.VK_CONTROL, KeyEvent.VK_SHIFT, KeyEvent.VK_W);
+                    }
+                } else if (result.startsWith("press")) {
+                    if (result.endsWith("delete")) {
+                        pressKeystroke(KeyEvent.VK_DELETE);
+                    } else if (result.endsWith("enter")) {
+                        pressKeystroke(KeyEvent.VK_ENTER);
+                    } else if (result.endsWith("escape")) {
+                        pressKeystroke(KeyEvent.VK_ESCAPE);
+                    } else if (result.endsWith("tab")) {
+                        pressKeystroke(KeyEvent.VK_TAB);
+                    } else if (result.endsWith("undo")) {
+                        pressKeystroke(KeyEvent.VK_CONTROL, KeyEvent.VK_Z);
+                    }
+                } else if (result.startsWith("next")) {
+                    if (result.endsWith("line")) {
+                        pressKeystroke(KeyEvent.VK_DOWN);
+                    } else if (result.endsWith("page")) {
+                        pressKeystroke(KeyEvent.VK_PAGE_DOWN);
+                    } else if (result.endsWith("method")) {
+                        pressKeystroke(KeyEvent.VK_ALT, KeyEvent.VK_DOWN);
+                    }
+                } else if (result.startsWith("previous")) {
+                    if (result.endsWith("line")) {
+                        pressKeystroke(KeyEvent.VK_UP);
+                    } else if (result.endsWith("page")) {
+                        pressKeystroke(KeyEvent.VK_PAGE_UP);
+                    } else if (result.endsWith("method")) {
+                        pressKeystroke(KeyEvent.VK_ALT, KeyEvent.VK_UP);
+                    }
+                } else if (result.startsWith("inspect code")) {
+                    pressKeystroke(KeyEvent.VK_ALT, KeyEvent.VK_SHIFT, KeyEvent.VK_I);
+                } if (result.startsWith("speech pause")) {
+                    while (true) {
+                        result = speechRecognizer.getResult().getHypothesis();
+                        if (result.equals("speech resume")) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void pressKeystroke(final int... keys) {
+        for (int key : keys) {
+            robot.keyPress(key);
+        }
+
+        for (int key : keys) {
+            robot.keyRelease(key);
+        }
+    }
+
+    private void invokeAction(final String actionShowSettings) {
+        try {
+            EventQueue.invokeAndWait(() -> {
+                AnAction anAction = ActionManager.getInstance().getAction(actionShowSettings);
+                anAction.actionPerformed(new AnActionEvent(null,
+                        DataManager.getInstance().getDataContext(),
+                        ActionPlaces.UNKNOWN, anAction.getTemplatePresentation(),
+                        ActionManager.getInstance(), 0));
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
