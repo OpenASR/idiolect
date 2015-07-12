cd7274bc2023db375b296d6fd93b32f2cee2ca9e
Alexey Kudinkin <alexey.kudinkin@gmail.com>
2015-07-12 13:49:50 +0300

    (PRJ): Extracted icons to resources

--- src/com/jetbrains/idear/Icons.java
+++ src/com/jetbrains/idear/Icons.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 
 public interface Icons {
 
-    Icon RECORD_START = IconLoader.getIcon("icons/start.png");
-    Icon RECORD_END = IconLoader.getIcon("icons/stop.png");
+    Icon RECORD_START = IconLoader.getIcon("/com.jetbrains.idear/icons/start.png");
+    Icon RECORD_END = IconLoader.getIcon("/com.jetbrains.idear/icons/stop.png");
 
 }
