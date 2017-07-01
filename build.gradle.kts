4d6d57a0d7bd5592e78e379c98f87cbef5de26fa
breandan <breandan.considine@gmail.com>
2017-06-30 21:10:45 -0400

    update martytts gradle build, fixes #30

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.gradle.script.lang.kotlin.*
-import org.jetbrains.intellij.IntelliJPluginExtension
-
 buildscript {
   repositories {
     maven { setUrl("http://dl.bintray.com/jetbrains/intellij-plugin-service") }
@@ -21,10 +18,7 @@ group = "com.jetbrains"
 version = "1.3"
 
 repositories {
-  mavenCentral()
-  maven {
-    setUrl("https://oss.jfrog.org/artifactory/repo/")
-  }
+  jcenter()
   maven {
     setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
   }
@@ -33,10 +27,7 @@ repositories {
 dependencies {
   compile("edu.cmu.sphinx:sphinx4-core:5prealpha-SNAPSHOT")
   compile("com.mashape.unirest:unirest-java:1.4.7")
-  compile("de.dfki.mary:marytts-runtime:5.2-SNAPSHOT")
-  compile("de.dfki.mary:marytts-lang-en:5.2-SNAPSHOT")
-  compile("de.dfki.mary:voice-cmu-slt-hsmm:5.2-SNAPSHOT")
   compile("org.codehaus.jettison:jettison:1.3.7")
-
+  compile("de.dfki.mary","voice-cmu-slt-hsmm", "5.2")
   testCompile("junit:junit:4.12")
 }
