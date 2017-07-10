2828d40dbd6f70e7edf6f7846438e9f4f5c49923
Nicholas Albion <nalbion@yahoo.com>
2017-07-11 02:30:41 +1000

    sketching out Lex interface

--- src/main/java/org/openasr/idear/nlp/lex/LexNlp.kt
+++ src/main/java/org/openasr/idear/nlp/lex/LexNlp.kt
@@ -10,6 +10,8 @@ class LexNlp {
     private lateinit var userId: String
 
     init {
+        // TODO: get userId from Cognito
+        userId = "TODO"
 //        lexAsync = AmazonLexRuntimeAsyncClientBuilder.standard().build()
         lex = AmazonLexRuntimeClientBuilder.standard()
 //                .withClientConfiguration()
@@ -18,6 +20,32 @@ class LexNlp {
                 .build()
     }
 
+    /**
+     * @param utterance - what the user said
+     * @param sessionAttributes - **new** context.
+     *                  eg: {ext:html,lang:js,ide:intellij, context:{}, scope:{}, toolWindow:"Terminal", PreviousAction:{}, ...}
+     *
+     * @throws NotFoundException
+     *         The resource (such as the Amazon Lex bot or an alias) that is referred to is not found.
+     * @throws BadRequestException
+     *         Request validation failed, there is no usable message in the context, or the bot build failed.
+     * @throws LimitExceededException
+     *         Exceeded a limit.
+     * @throws InternalFailureException
+     *         Internal service error. Retry the call.
+     * @throws ConflictException
+     *         Two clients are using the same AWS account, Amazon Lex bot, and user ID.
+     * @throws DependencyFailedException
+     *         One of the downstream dependencies, such as AWS Lambda or Amazon Polly, threw an exception. For example,
+     *         if Amazon Lex does not have sufficient permissions to call a Lambda function, it results in Lambda
+     *         throwing an exception.
+     * @throws BadGatewayException
+     *         Either the Amazon Lex bot is still building, or one of the dependent services (Amazon Polly, AWS Lambda)
+     *         failed with an internal service error.
+     * @throws LoopDetectedException
+     *         Lambda fulfilment function returned <code>DelegateDialogAction</code> to Amazon Lex without changing any
+     *         slot values.
+     */
     fun processUtterance(utterance: String, sessionAttributes: Map<String, String>? = null) {
         val request = PostTextRequest()
                 .withBotName("idear")
@@ -29,9 +57,11 @@ class LexNlp {
         val response = lex.postText(request)
         println(response)
 
-        // ElicitIntent, ConfirmIntent, ElicitSlot, Fulfilled, ReadyForFulfillment, Failed
+        // Close      - Fulfilled or Failed   (ReadyForFulfillment?)
+        // Incomplete - ElicitIntent, ConfirmIntent, ElicitSlot
         response.dialogState
         // ApplyInspection | ApplyFile/Code/LiveTemplate | ApplyAction | ExecuteUserScript
+        //
         response.intentName
         response.message
         /* {genericAttachments:[
@@ -46,10 +76,11 @@ class LexNlp {
         response.slotToElicit
         // {className:"MyComponent", fileName:"MyComponent.tsx", functionName:...}
         response.slots
-        // {ext:html,lang:js,context:{}, scope:{},toolWindow:"Terminal" ,PreviousAction:{},...}
+        // {ext:html,lang:js,context:{},ide:intellij, scope:{},toolWindow:"Terminal" ,PreviousAction:{},...}
         response.sessionAttributes
 
-//        response.dialogState
+
+        //
     }
 }
 
