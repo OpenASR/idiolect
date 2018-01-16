c41def0c069087d8e60c0c8f75a315372f17bde8
breandan <breandan.considine@gmail.com>
2018-01-16 00:13:43 -0500

    remove unnecessary modifier

--- src/main/java/org/openasr/idear/actions/IdearAction.kt
+++ src/main/java/org/openasr/idear/actions/IdearAction.kt
@@ -3,6 +3,6 @@ package org.openasr.idear.actions
 import com.intellij.openapi.actionSystem.*
 import org.openasr.idear.ide.IDEService.invokeAction
 
-abstract open class IdearAction : AnAction() {
+abstract class IdearAction : AnAction() {
     fun invoke() = invokeAction(ActionManager.getInstance().getId(this))
 }
