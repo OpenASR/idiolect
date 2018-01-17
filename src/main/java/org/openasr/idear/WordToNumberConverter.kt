3beafe9d2d7d2156d925a82f20eec38cd82136e8
breandan <breandan.considine@gmail.com>
2018-01-17 15:21:41 -0500

    refactor number recognizer

--- src/main/java/org/openasr/idear/WordToNumberConverter.kt
+++ src/main/java/org/openasr/idear/WordToNumberConverter.kt
@@ -7,38 +7,43 @@ object WordToNumberConverter {
     fun getNumber(input: String): Int {
         var accumulator = 0
         var total = 0
-        val words = input.trim { it <= ' ' }.split("\\s+".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+        val words = input.trim { it <= ' ' }
+                .split("\\s+".toRegex())
+                .dropLastWhile { it.isEmpty() }
+                .toTypedArray()
 
         for (word in words) {
+            accumulator += when (word) {
+                "one" -> 1
+                "two" -> 2
+                "three" -> 3
+                "four" -> 4
+                "five" -> 5
+                "six" -> 6
+                "seven" -> 7
+                "eight" -> 8
+                "nine" -> 9
+                "ten" -> 10
+                "eleven" -> 11
+                "twelve" -> 12
+                "thirteen" -> 13
+                "fourteen" -> 14
+                "fifteen" -> 15
+                "sixteen" -> 16
+                "seventeen" -> 17
+                "eighteen" -> 18
+                "nineteen" -> 19
+                "twenty" -> 20
+                "thirty" -> 30
+                "forty" -> 40
+                "fifty" -> 50
+                "sixty" -> 60
+                "seventy" -> 70
+                "eighty" -> 80
+                "ninety" -> 90
+                else -> 0
+            }
             when (word) {
-                "zero" -> accumulator += 0
-                "one" -> accumulator += 1
-                "two" -> accumulator += 2
-                "three" -> accumulator += 3
-                "four" -> accumulator += 4
-                "five" -> accumulator += 5
-                "six" -> accumulator += 6
-                "seven" -> accumulator += 7
-                "eight" -> accumulator += 8
-                "nine" -> accumulator += 9
-                "ten" -> accumulator += 10
-                "eleven" -> accumulator += 11
-                "twelve" -> accumulator += 12
-                "thirteen" -> accumulator += 13
-                "fourteen" -> accumulator += 14
-                "fifteen" -> accumulator += 15
-                "sixteen" -> accumulator += 16
-                "seventeen" -> accumulator += 17
-                "eighteen" -> accumulator += 18
-                "nineteen" -> accumulator += 19
-                "twenty" -> accumulator += 20
-                "thirty" -> accumulator += 30
-                "forty" -> accumulator += 40
-                "fifty" -> accumulator += 50
-                "sixty" -> accumulator += 60
-                "seventy" -> accumulator += 70
-                "eighty" -> accumulator += 80
-                "ninety" -> accumulator += 90
                 "hundred" -> accumulator *= 100
                 "thousand" -> {
                     accumulator *= 1000
