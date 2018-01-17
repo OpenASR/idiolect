19a56b6a16733a1a5723fdaef4a9c9756272ab4d
breandan <breandan.considine@gmail.com>
2018-01-17 15:27:00 -0500

    bump kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
 
 plugins {
     id("org.jetbrains.intellij") version "0.2.17"
-    id("org.jetbrains.kotlin.jvm") version "1.2.10"
+    id("org.jetbrains.kotlin.jvm") version "1.2.20"
 }
 
 intellij {
