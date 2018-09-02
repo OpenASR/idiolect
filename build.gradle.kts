3ac7809c7a9690dee08b845162a68fab8d3cd67b
breandan.considine <breandan.considine@gmail.com>
2018-09-02 13:53:24 -0400

    bump versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("org.jetbrains.intellij") version "0.3.4"
-    id("org.jetbrains.kotlin.jvm") version "1.2.50"
+    id("org.jetbrains.intellij") version "0.3.7"
+    id("org.jetbrains.kotlin.jvm") version "1.2.61"
 }
 
 intellij {
