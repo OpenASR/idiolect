59dc492e4e0e45cf4fed6626470ac864811fb9ab
breandan <bre@ndan.co>
2020-05-10 23:13:15 -0400

    implements #58

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.intellij.tasks.PatchPluginXmlTask
 
 plugins {
     idea apply true
@@ -36,11 +37,10 @@ tasks {
 //        val intellijPublishToken: String? by project
 //        token(intellijPublishToken)
 //    }
-//
-//    withType<PatchPluginXmlTask> {
-//        sinceBuild("201.6668.0")
-//        changeNotes(fetchChangeNotes())
-//    }
+
+    withType<PatchPluginXmlTask> {
+        sinceBuild("201.6668.0")
+    }
 }
 
 repositories {
@@ -54,12 +54,12 @@ dependencies {
     implementation("net.sourceforge.javaflacencoder:java-flac-encoder:0.3.7")
     implementation("edu.cmu.sphinx:sphinx4-core:5prealpha-SNAPSHOT")
     implementation("com.mashape.unirest:unirest-java:1.4.9")
-    implementation("org.codehaus.jettison:jettison:1.4.0")
+    implementation("org.codehaus.jettison:jettison:1.4.1")
     implementation("de.dfki.mary:voice-cmu-slt-hsmm:5.2")
     implementation("com.amazonaws:aws-java-sdk-cognitoidentity:1.11.340")
     implementation("com.amazonaws:aws-java-sdk-lex:1.11.340")
     implementation("com.amazonaws:aws-java-sdk-polly:1.11.340")
     implementation("com.googlecode.soundlibs:jlayer:1.0.1.4")
     implementation("com.google.cloud:google-cloud-speech:0.32.0-alpha")
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13")
 }
--- src/main/java/org/openasr/idear/nlp/NlpParserService.kt
+++ src/main/java/org/openasr/idear/nlp/NlpParserService.kt
@@ -1,19 +1,21 @@
 package org.openasr.idear.nlp
 
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.extensions.PluginId
 import opennlp.tools.cmdline.parser.ParserTool
 import opennlp.tools.parser.*
 import org.jetbrains.annotations.TestOnly
-import org.openasr.idear.Idear
 import java.io.*
 
 class NlpParserService @TestOnly constructor(path: String?) : ParserService() {
     private lateinit var parser: Parser
     private val logger = Logger.getInstance(javaClass)
+    val plugin by lazy { PluginManagerCore.getPlugin(PluginId.getId("com.jetbrains.idear"))!! }
 
     private val modelInputStream: InputStream =
             if (path != null) FileInputStream(path)
-            else Idear.plugin.pluginClassLoader.getResourceAsStream("en-parser-chunking.bin")
+            else plugin.pluginClassLoader.getResourceAsStream("en-parser-chunking.bin")
 
     override fun parseSentence(sentence: String): Parse? =
             ParserTool.parseLine(sentence, parser, 1).apply { assert(size == 1) }[0]
