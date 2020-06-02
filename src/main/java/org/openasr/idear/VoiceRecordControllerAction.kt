625881fbe71f3b88f55c827b1ceda1cc401cca08
breandan <bre@ndan.co>
2020-06-02 18:17:18 -0400

    fix AceJump build

--- src/main/java/org/openasr/idear/VoiceRecordControllerAction.kt
+++ src/main/java/org/openasr/idear/VoiceRecordControllerAction.kt
@@ -1,27 +1,30 @@
 package org.openasr.idear
 
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.ServiceManager
 import org.acejump.config.AceConfig
+import org.acejump.config.AceSettings
 import org.openasr.idear.actions.IdearAction
 import org.openasr.idear.asr.ASRService
 import org.openasr.idear.presentation.Icons
 
 object VoiceRecordControllerAction : IdearAction() {
     @Volatile private var isRecording = false
-    private var aceJumpDefaults = AceConfig.state.allowedChars
 
     override fun actionPerformed(anActionEvent: AnActionEvent) {
+      val settings = ServiceManager.getService(AceConfig::class.java).state
+      val aceJumpDefaults =settings.allowedChars
         if (isRecording) {
             isRecording = false
             templatePresentation.icon = Icons.RECORD_START
             isDefaultIcon = false
-            AceConfig.state.allowedChars = aceJumpDefaults
+            settings.allowedChars = aceJumpDefaults
             ASRService.deactivate()
         } else {
             isRecording = true
             templatePresentation.icon = Icons.RECORD_END
             isDefaultIcon = false
-            AceConfig.state.allowedChars = "1234567890"
+            settings.allowedChars = "1234567890"
             ASRService.activate()
         }
     }
