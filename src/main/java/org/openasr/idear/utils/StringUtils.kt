67d5ebd7592665b0835ac1a0d61fb2cc12ca25fb
breandan <bre@ndan.co>
2022-12-26 16:17:06 +1100

    simplify StringUtils

--- src/main/java/org/openasr/idear/utils/StringUtils.kt
+++ src/main/java/org/openasr/idear/utils/StringUtils.kt
@@ -4,17 +4,18 @@ private val toCamelCaseRegex = Regex("\\s([a-z])")
 private val toUpperCamelCaseRegex = Regex("\\s?\\b([a-z])")
 private val fromCamelCaseRegex = Regex("([A-Z0-9]+)([a-z]*)")
 
-fun String.capitalizeWord(): CharSequence {
-    val start = this[0]
-    return if (start.isUpperCase()) this
-    else (StringBuilder(SingleChar(start.uppercaseChar())).append(substring(1)))
-}
+fun String.capitalizeWord(): CharSequence =
+    if (first().isUpperCase()) this
+    else (StringBuilder(SingleChar(first().uppercaseChar())).append(substring(1)))
 
-fun String.toCamelCase() = this.replace(toCamelCaseRegex) { SingleChar(it.groups[1]!!.value[0].uppercaseChar()) }
+fun String.toCamelCase() =
+    replace(toCamelCaseRegex) { SingleChar(it.groups[1]!!.value[0].uppercaseChar()) }
 
-fun String.toUpperCamelCase() = this.replace(toUpperCamelCaseRegex) { SingleChar(it.groups[1]!!.value[0].uppercaseChar()) }
+fun String.toUpperCamelCase() =
+    replace(toUpperCamelCaseRegex) { SingleChar(it.groups[1]!!.value[0].uppercaseChar()) }
 
-fun String.expandCamelCase() = StringBuilder(substring(1))
+fun String.expandCamelCase() =
+    StringBuilder(substring(1))
         .insert(0, this[0].lowercaseChar())
         .replace(fromCamelCaseRegex) { m ->
             if (m.groups[1]?.value.isNullOrEmpty())
@@ -24,16 +25,15 @@ fun String.expandCamelCase() = StringBuilder(substring(1))
                     .append(m.groups[2]!!.value)
         }
 
-fun String.splitCamelCase(): Sequence<String> {
-    return fromCamelCaseRegex.findAll(capitalizeWord())
-            .map {
-                val word = StringBuilder(it.groups[2]!!.value)
-                if (!it.groups[1]?.value.isNullOrEmpty()) {
-                    word.insert(0, it.groups[1]!!.value.lowercase())
-                }
-                word.toString()
+fun String.splitCamelCase(): Sequence<String> =
+    fromCamelCaseRegex.findAll(capitalizeWord())
+        .map {
+            val word = StringBuilder(it.groups[2]!!.value)
+            if (!it.groups[1]?.value.isNullOrEmpty()) {
+                word.insert(0, it.groups[1]!!.value.lowercase())
             }
-}
+            word.toString()
+        }
 
 class SingleChar(private val char: Char) : CharSequence {
     override val length = 1
