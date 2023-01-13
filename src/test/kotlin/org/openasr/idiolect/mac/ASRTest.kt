2ef8f51bb52adb04fc8159450b991b4276efba28
breandan <bre@ndan.co>
2023-01-13 12:49:34 -0500

    measure WER across voices

--- src/test/kotlin/org/openasr/idiolect/mac/ASRTest.kt
+++ src/test/kotlin/org/openasr/idiolect/mac/ASRTest.kt
@@ -5,7 +5,7 @@ import org.vosk.*
 import java.io.File
 
 class ASRTest {
-    fun String.measureWER(): Float {
+    fun String.measureWER(voice: String): Float {
         // Run say command
         val outputFile = "/tmp/test"
 
@@ -24,7 +24,9 @@ class ASRTest {
           .let { (1 - it.numCorrect / it.referenceLength.toFloat()) }
     }
 
-  val modelName =
+    val voices = setOf("Eddy", "Flo", "Reed", "Rocko", "Samantha", "Sandy", "Shelley", "Fred")
+
+    val modelName =
     "vosk-model-en-us-0.22"
 //  "vosk-model-en-us-0.22-lgraph"
 //  "vosk-model-small-en-us-0.15"
@@ -33,7 +35,6 @@ class ASRTest {
     fun File.transcribeWavFile(): String {
         // Stream wav
         val bytes = readBytes()
-
         rec.acceptWaveForm(bytes, bytes.size)
         return rec.result.drop(14).dropLast(3)
     }
@@ -42,7 +43,17 @@ class ASRTest {
     fun testUtterances() {
       File("/tmp/utterances.txt").readLines().filter { it.isNotBlank() }
         .shuffled().take(100) // Sample 100 utterances
-        .map { it.trim().measureWER() }.average()
-        .let { throw Exception("Average word error rate: $it") }
+        .flatMap { u -> voices.map { it to u } }
+        .map { (voice, utterance) -> voice to utterance.trim().measureWER(voice) }
+        .groupBy { it.first }.mapValues { it.value.map { it.second }.average() }
+        //Eddy     :: 0.24
+        //Flo      :: 0.19
+        //Reed     :: 0.22
+        //Rocko    :: 0.24
+        //Samantha :: 0.29
+        //Sandy    :: 0.24
+        //Shelley  :: 0.26
+        //Fred     :: 0.27
+        .let { throw Exception("Average word error rate:\n${it.entries.joinToString("\n") { it.key + " :: " + it.value }}") }
     }
 }
