005dfa195655b7ebd2c6cf5ef7d8892e66d33c6c
breandan <bre@ndan.co>
2023-01-13 00:28:26 -0500

    test now passes

--- src/test/kotlin/org/openasr/idiolect/mac/ASRTest.kt
+++ src/test/kotlin/org/openasr/idiolect/mac/ASRTest.kt
@@ -2,21 +2,35 @@ package org.openasr.idiolect.mac
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.openasr.idiolect.asr.vosk.VoskAsr
 import org.vosk.*
-import java.io.File
+import java.io.*
 
 class ASRTest {
     @Test
     fun testTTSToASR() {
         // Run say command
-        "say -o /tmp/hello.wav --channels=1 --data-format=LEF32@16000 --rate=60 the"
-            .let { ProcessBuilder(it.split(" ")).start().waitFor() }
+        val utterance = "the quick brown fox jumped over the lazy dog"
+        val outputFile = "/tmp/test"
+
+        ("say -o $outputFile.aif".split(" ") + "\"$utterance\"")
+          .let { ProcessBuilder(it).start().waitFor() }
+
+        ("ffmpeg -y -i $outputFile.aif $outputFile.wav".split(" ")) // ffmpeg defaults to pcm_s16le for WAV
+          .let { ProcessBuilder(it).start().waitFor() }
 
         // Read AIFF file
-        val testUtterance = File("/tmp/hello.wav").readBytes()
-        val rec = Recognizer(Model("/Users/breandan/.idiolect/vosk-model-en-us-0.22-lgraph"), 16000f)
-        rec.acceptWaveForm(testUtterance, testUtterance.size)
-        assertEquals("the", rec.result.drop(14).dropLast(3))
+        val testUtterance = File("$outputFile.wav").transcribeWavFile()
+        assertEquals(utterance, testUtterance)
     }
+
+    fun File.transcribeWavFile(): String {
+        // Stream wav
+        val rec = Recognizer(Model("${System.getProperty("user.home")}/.idiolect/vosk-model-en-us-0.22"), 16000f)
+
+        val bytes = readBytes()
+
+        rec.acceptWaveForm(bytes, bytes.size)
+        return rec.result.drop(14).dropLast(3)
+    }
+
 }
