634641febd2c8638df096bca63c6b266645dda10
Nicholas Albion <nalbion@yahoo.com>
2023-05-03 04:37:32 +1000

    fix for ExtensionPointName compatibility

--- src/main/java/org/openasr/idiolect/settings/ExtensionManager.kt
+++ src/main/java/org/openasr/idiolect/settings/ExtensionManager.kt
@@ -6,10 +6,10 @@ import org.openasr.idiolect.nlp.NlpProvider
 import org.openasr.idiolect.tts.TtsProvider
 
 object ExtensionManager {
-    internal val asrSystemEp: ExtensionPointName<AsrSystem> = ExtensionPointName.create("org.openasr.idiolect.asrSystem")
-    internal val asrEp: ExtensionPointName<AsrProvider> = ExtensionPointName.create("org.openasr.idiolect.asrProvider")
-    internal val ttsEp: ExtensionPointName<TtsProvider> = ExtensionPointName.create("org.openasr.idiolect.ttsProvider")
-    internal val nlpEp: ExtensionPointName<NlpProvider> = ExtensionPointName.create("org.openasr.idiolect.nlpProvider")
+    internal val asrSystemEp: ExtensionPointName<AsrSystem> = ExtensionPointName("org.openasr.idiolect.asrSystem")
+    internal val asrEp: ExtensionPointName<AsrProvider> = ExtensionPointName("org.openasr.idiolect.asrProvider")
+    internal val ttsEp: ExtensionPointName<TtsProvider> = ExtensionPointName("org.openasr.idiolect.ttsProvider")
+    internal val nlpEp: ExtensionPointName<NlpProvider> = ExtensionPointName("org.openasr.idiolect.nlpProvider")
 
     internal var asrSelector: ExtensionSelector<AsrProvider> = ExtensionSelector(asrEp)
     internal var ttsSelector: ExtensionSelector<TtsProvider> = ExtensionSelector(ttsEp)
