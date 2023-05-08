52d3de914f5e5822f4b6556b4024ef6a2e8adda0
Nicholas Albion <nalbion@yahoo.com>
2023-05-08 13:43:22 +1000

    restored proper implementation of sendCompletion()

--- src/main/java/org/openasr/idiolect/nlp/ai/AiService.kt
+++ src/main/java/org/openasr/idiolect/nlp/ai/AiService.kt
@@ -43,17 +43,17 @@ class AiService {
 
         publisher.onUserPrompt(prompt)
 
-//        val choices = client.sendCompletion(prompt)
-        val choices = listOf("""
-            Here is the code for `AiService`:
-            ```
-            class AiService {
-                init {
-                    println("hello world")
-                }
-            }
-            ```
-            """.trimIndent())
+        val choices = client.sendCompletion(prompt)
+//        val choices = listOf("""
+//            Here is the code for `AiService`:
+//            ```
+//            class AiService {
+//                init {
+//                    println("hello world")
+//                }
+//            }
+//            ```
+//            """.trimIndent())
 
         publisher.onAiResponse(choices)
     }
