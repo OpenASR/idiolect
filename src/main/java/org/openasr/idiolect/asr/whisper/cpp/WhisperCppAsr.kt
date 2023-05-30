5225501adddbc8f579b40860dbb595111e0af692
Nicholas Albion <nalbion@yahoo.com>
2023-05-30 15:01:19 +1000

    WIP - whisper-cpp needs to run in a thread.

--- src/main/java/org/openasr/idiolect/asr/whisper/cpp/WhisperCppAsr.kt
+++ src/main/java/org/openasr/idiolect/asr/whisper/cpp/WhisperCppAsr.kt
@@ -1,14 +1,15 @@
 package org.openasr.idiolect.asr.whisper.cpp
 
+import ai.grazie.utils.Time
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.logger
 import io.github.ggerganov.whispercpp.WhisperCpp
 import io.github.ggerganov.whispercpp.params.WhisperFullParams
 import io.github.ggerganov.whispercpp.params.WhisperSamplingStrategy
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.first
 import org.openasr.idiolect.asr.AsrProvider
 import org.openasr.idiolect.asr.AsrSystemStateListener
 import org.openasr.idiolect.asr.offline.OfflineAsr
@@ -16,11 +17,17 @@ import org.openasr.idiolect.asr.whisper.cpp.settings.WhisperCppConfig
 import org.openasr.idiolect.asr.whisper.cpp.settings.WhisperCppConfigurable
 import org.openasr.idiolect.asr.whisper.cpp.settings.WhisperCppModelManager
 import org.openasr.idiolect.nlp.NlpRequest
+import org.openasr.idiolect.recognizer.CustomMicrophone
 
 class WhisperCppAsr : OfflineAsr<WhisperCppConfigurable>(WhisperCppModelManager) {
     private val log = logger<WhisperCppAsr>()
+    val SAMPLES_TO_PROCESS = 16 * 1024 * 4 //  CustomMicrophone.format.sampleRate.toInt() * 10 // 2048 * 16
+    val MAX_SAMPLE_VALUE = 32767.0f
+
     private var grammar: Array<String>? = null
     private var whisperParams: WhisperFullParams? = null
+    private val _speechResults = MutableSharedFlow<NlpRequest?>()
+    val speechResults: SharedFlow<NlpRequest?> = _speechResults
 
     override fun displayName(): String = "whisper.cpp"
 
@@ -57,7 +64,7 @@ class WhisperCppAsr : OfflineAsr<WhisperCppConfigurable>(WhisperCppModelManager)
         whisperParams.printProgress(false)
         whisperParams.suppressNonSpeechTokens(true)
 //        whisperParams.no_speech_thold = 0.75f   // default 0.6
-        whisperParams.setBestOf(4)
+//        whisperParams.setBestOf(4)
 //        whisperParams.setBeamSizeAndPatience(4, -1.0f)
 
         this.whisperParams = whisperParams
@@ -75,42 +82,72 @@ class WhisperCppAsr : OfflineAsr<WhisperCppConfigurable>(WhisperCppModelManager)
 //        Whisper.
     }
 
-    val SAMPLES_TO_PROCESS = 2048 * 8
-    val MAX_SAMPLE_VALUE = 32767.0f
+    override fun startRecognition(): Boolean {
+        val started = super.startRecognition()   // start the mic
 
-    override fun waitForSpeech(): NlpRequest? {
         var nbytes: Int
-        val b = ByteArray(SAMPLES_TO_PROCESS shl 1)
-        val floats = FloatArray(SAMPLES_TO_PROCESS)
+        val audioData = ByteArray(SAMPLES_TO_PROCESS shl 1)
 
-        val stopWords = AsrProvider.stopWords(grammar)
-
-        while (microphone.stream.read(b).also { nbytes = it } > 0 && listening) {
-//            var j = 0
-//            for (i in 0 until nbytes step 2) {
-            for ((j, i) in (0 until nbytes step 2).withIndex()) {
-                val sample = (((b[i + 1].toInt() shl 8) + (b[i].toInt() and 0xff)) / MAX_SAMPLE_VALUE).coerceIn(-1f..1f)
-
-                floats[j] = sample
+        CoroutineScope(Dispatchers.IO).launch {
+            while (microphone.stream.read(audioData).also { nbytes = it } > 0 && listening) {
+//                if (vad.isSpeech(audioData)) {
+                    CoroutineScope(Dispatchers.Default).launch {
+                        val result = processSpeech(audioData, nbytes) // Blocking call to process speech
+                        if (result != null) {
+                            _speechResults.emit(result)
+                        }
+                    }
+//                }
             }
-
-            val nlpRequest = runBlocking {
-                var result = whisper.fullTranscribe(whisperParams, floats)
-//                println("results: '$result'")
-                result = result.replace(",", "")
-                    .replace(Regex("[.?!]$"), "")
-                    .lowercase()
-
-                result = AsrProvider.removeStopWords(result, stopWords)
-
-                return@runBlocking if (result.isEmpty()) null else NlpRequest(listOf(result))
-            }
-
-            if (nlpRequest != null) {
-                return nlpRequest
-            }
+            println("Stopped recognition coroutine")
+            _speechResults.emit(null)
         }
 
-        return null
+        return started
+    }
+
+//    override fun stopRecognition(): Boolean {
+//        val stopped = super.stopRecognition()    // stop the mic
+//        return stopped
+//    }
+
+    private fun processSpeech(audioData: ByteArray, nbytes: Int): NlpRequest? {
+        val floats = FloatArray(SAMPLES_TO_PROCESS)
+        val stopWords = AsrProvider.stopWords(grammar)
+        var start = Time.epochMillis()
+
+        for ((j, i) in (0 until nbytes step 2).withIndex()) {
+            val sample = (((audioData[i + 1].toInt() shl 8) + (audioData[i].toInt() and 0xff)) / MAX_SAMPLE_VALUE).coerceIn(-1f..1f)
+
+            floats[j] = sample
+        }
+
+//        val nlpRequest = runBlocking {
+            println("audio ${Time.epochMillis() - start}ms")
+            start = Time.epochMillis()
+
+            var result = whisper.fullTranscribe(whisperParams, floats)
+            println("results: '$result', ${Time.epochMillis() - start}ms")
+            start = Time.epochMillis()
+
+            result = result.replace(",", "")
+                .replace(Regex("[.?!]$"), "")
+                .lowercase()
+
+            result = AsrProvider.removeStopWords(result, stopWords)
+
+            return if (result.isEmpty()) null else NlpRequest(listOf(result))
+//            return@runBlocking if (result.isEmpty()) null else NlpRequest(listOf(result))
+//        }
+//
+//        if (nlpRequest != null) {
+//            return nlpRequest
+//        }
+    }
+
+    override fun waitForSpeech(): NlpRequest? {
+        return runBlocking {
+            speechResults.first()
+        }
     }
 }
